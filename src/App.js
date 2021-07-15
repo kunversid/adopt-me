4e20b3fa77f22c7de20b5bc0a16402e921251e39
Kunver Siddharth <kunversiddharth@outlook.com>
2021-07-15 20:54:47 +0530

    Add Prettier and ESLint

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h2", {}, "Bruno"),
-        React.createElement("h3", {}, "Dog"),
-        React.createElement("h3", {}, "Himalayan Sheepdog"),
-    ])
-}
+        React.createElement("h2", {}, props.name),
+        React.createElement("h3", {}, props.animal),
+        React.createElement("h3", {}, props.breed),
+    ]);
+};
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
-        ]
-    );
+    return React.createElement("div", {}, [
+        React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Bruno",
+            animal: "Dog",
+            breed: "Himalayan Sheepdog",
+        }),
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel",
+        }),
+        React.createElement(Pet, { name: "Tiger", animal: "Dog", breed: "Pug" }),
+    ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
